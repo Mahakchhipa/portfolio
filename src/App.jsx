35d2002512beb92d5cbff1ccc3beb0e25d46fda3
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-06-27 22:33:07 +0530

    React project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+       <h1 className=' text-center font-bold py-5 text-4xl text-red-600 '> Hello Mahak </h1>
+    </div>
+  )
+}
+
+export default App
